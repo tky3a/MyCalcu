907289d1cafb1d61de462aed6cff75e2893a1040
tky <takayamac@icloud.com>
2021-12-21 22:19:52 +0900

    電卓アプリ作成中

---
+++ app/src/main/java/ja/tutorial/mycalcu/MainActivity.kt
@@ -0,0 +1,17 @@
+package ja.tutorial.mycalcu
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this, "ボタンクリック", Toast.LENGTH_LONG).show()
+    }
+}
